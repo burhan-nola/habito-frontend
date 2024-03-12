8ecd9baa259c793d8fb79a5dcd8440ab62bc6b92
= <=>
2024-03-12 10:23:27 +0700

    add redirect url

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,14 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import style from "../css/bulbTask.module.css";
-import RedBulbCheck from "./bulb/check/RedBulbCheck";
-import GreenBulbCheck from "./bulb/check/GreenBulbCheck";
-import BlueBulbCheck from "./bulb/check/BlueBulbCheck";
-import YellowBulbCheck from "./bulb/check/YellowBulbCheck";
-import RedBulbCross from "./bulb/cross/RedBulbCross";
-import GreenBulbCross from "./bulb/cross/GreenBulbCross";
-import BlueBulbCross from "./bulb/cross/BlueBulbCross";
-import YellowBulbCross from "./bulb/cross/YellowBulbCross";
 import axios, { AxiosResponse } from "axios";
 import Bulb from "./Bulb";
 
